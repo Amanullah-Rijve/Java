44886684e870a04def84fe3c66d8f2a76edd036d
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-14 22:14:43 +0600

    Add files via upload

---
+++ First.java
@@ -0,0 +1,12 @@
+// Lets code Arithmatic
+
+public class First{
+    public static void main(String[] args){
+        System.out.println(3+3);
+        System.out.println(3-3);
+        System.out.println(3*3);
+        System.out.println(3/3);
+        System.out.println(3%3);
+    }
+}
+

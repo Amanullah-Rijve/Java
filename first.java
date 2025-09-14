e30953dba68c116de1ae1519751f3230dffa56c9
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-15 01:10:05 +0600

    Add files via upload

---
+++ first.java
@@ -0,0 +1,9 @@
+// first lesson
+
+public class first{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("I am learning Java");
+        System.out.println("I hate Java");
+    }
+}

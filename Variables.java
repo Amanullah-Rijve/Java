53155e7ebb2161c4b4bb8de1285059f2db054bbd
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-14 22:15:21 +0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,17 @@
+//! Veriables in Java
+
+public class Variables{
+    public static void main(String[] args){
+        int myNum = 5;
+        float Num = 4.90f;
+        boolean myBol = true;
+        char myC  ='A';
+        String text= "Hello Diu";
+        System.out.println(myNum);
+        System.out.println(Num);
+        System.out.println(myBol);
+        System.out.println(myC);
+        System.out.println(text);
+    }
+}
+

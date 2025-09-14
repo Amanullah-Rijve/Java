c31eef0042332a1a0f177773d89e6124b8beb6f9
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-15 01:10:21 +0600

    Add files via upload

---
+++ Num.java
@@ -0,0 +1,10 @@
+// printing numbers
+
+public class Num{
+    public static void main(String[] args){
+        System.out.println(3);
+        System.out.println(3+3);
+        System.out.println(3-3);
+        System.out.println(3*3);
+    }
+}

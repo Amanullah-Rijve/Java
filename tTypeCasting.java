4bca7a67aaf08c554439c64172da232d4c6565cf
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-17 01:44:51 +0600

    Add files via upload

---
+++ tTypeCasting.java
@@ -0,0 +1,19 @@
+public class tTypeCasting{
+    public static void main(String[] args){
+        int myNum = 9;
+        double myDou = myNum;
+
+        System.out.println(myDou);
+        System.out.println(myNum);
+    }
+}
+
+public class tTypeCasting{
+    public static void main(String[] args){
+        double myDouble = 56.984d;
+        int myNum = (int)myDouble;
+
+        System.out.println(myDouble);
+        System.out.println(myNum);
+    }
+}

ba477ac9de0f38578facad3434beeb30afacc4a2
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-26 13:55:19 +0600

    Add files via upload

---
+++ Multi_Table.java
@@ -0,0 +1,189 @@
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table Of 1 ===");
+        System.out.println("1x1= " + myMethod(1,1));
+        System.out.println("1x2= " +  myMethod(1,2));
+        System.out.println("1x3= " +  myMethod(1,3));
+        System.out.println("1x4= " +  myMethod(1,4));
+        System.out.println("1x5= " +  myMethod(1,5));
+        System.out.println("1x6= " +  myMethod(1,6));
+        System.out.println("1x7= " +  myMethod(1,7));
+        System.out.println("1x8= " +  myMethod(1,8));
+        System.out.println("1x9= " +  myMethod(1,9));
+        System.out.println("1x10= " + myMethod(1,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("===  Multipication Table of 2 ===");
+        System.out.println("2x1= " + myMethod(2,1));
+        System.out.println("2x2= " +  myMethod(2,2));
+        System.out.println("2x3= " +  myMethod(2,3));
+        System.out.println("2x4= " +  myMethod(2,4));
+        System.out.println("2x5= " +  myMethod(2,5));
+        System.out.println("2x6= " +  myMethod(2,6));
+        System.out.println("2x7= " +  myMethod(2,7));
+        System.out.println("2x8= " +  myMethod(2,8));
+        System.out.println("2x9= " +  myMethod(2,9));
+        System.out.println("2x10= " + myMethod(2,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table of 3 ===");
+        System.out.println("3x1= " +  myMethod(3,1));
+        System.out.println("3x2= " +  myMethod(3,2));
+        System.out.println("3x3= " +  myMethod(3,3));
+        System.out.println("3x4= " +  myMethod(3,4));
+        System.out.println("3x5= " +  myMethod(3,5));
+        System.out.println("3x6= " +  myMethod(3,6));
+        System.out.println("3x7= " +  myMethod(3,7));
+        System.out.println("3x8= " +  myMethod(3,8));
+        System.out.println("3x9= " +  myMethod(3,9));
+        System.out.println("3x10= " + myMethod(3,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table of 4 ===");
+        System.out.println("4x1= " +  myMethod(4,1));
+        System.out.println("4x2= " +  myMethod(4,2));
+        System.out.println("4x3= " +  myMethod(4,3));
+        System.out.println("4x4= " +  myMethod(4,4));
+        System.out.println("4x5= " +  myMethod(4,5));
+        System.out.println("4x6= " +  myMethod(4,6));
+        System.out.println("4x7= " +  myMethod(4,7));
+        System.out.println("4x8= " +  myMethod(4,8));
+        System.out.println("4x9= " +  myMethod(4,9));
+        System.out.println("4x10= " + myMethod(4,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table of 5 ===");
+        System.out.println("5x1= " +  myMethod(5,1));
+        System.out.println("5x2= " +  myMethod(5,2));
+        System.out.println("5x3= " +  myMethod(5,3));
+        System.out.println("5x4= " +  myMethod(5,4));
+        System.out.println("5x5= " +  myMethod(5,5));
+        System.out.println("5x6= " +  myMethod(5,6));
+        System.out.println("5x7= " +  myMethod(5,7));
+        System.out.println("5x8= " +  myMethod(5,8));
+        System.out.println("5x9= " +  myMethod(5,9));
+        System.out.println("5x10= " + myMethod(5,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table of 6 ===");
+        System.out.println("6x1= " +  myMethod(6,1));
+        System.out.println("6x2= " +  myMethod(6,2));
+        System.out.println("6x3= " +  myMethod(6,3));
+        System.out.println("6x4= " +  myMethod(6,4));
+        System.out.println("6x5= " +  myMethod(6,5));
+        System.out.println("6x6= " +  myMethod(6,6));
+        System.out.println("6x7= " +  myMethod(6,7));
+        System.out.println("6x8= " +  myMethod(6,8));
+        System.out.println("6x9= " +  myMethod(6,9));
+        System.out.println("6x10= " + myMethod(6,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table of 7 ===");
+        System.out.println("7x1= " +  myMethod(7,1));
+        System.out.println("7x2= " +  myMethod(7,2));
+        System.out.println("7x3= " +  myMethod(7,3));
+        System.out.println("7x4= " +  myMethod(7,4));
+        System.out.println("7x5= " +  myMethod(7,5));
+        System.out.println("7x6= " +  myMethod(7,6));
+        System.out.println("7x7= " +  myMethod(7,7));
+        System.out.println("7x8= " +  myMethod(7,8));
+        System.out.println("7x9= " +  myMethod(7,9));
+        System.out.println("7x10= " + myMethod(7,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table of 8 ===");
+        System.out.println("8x1= " +  myMethod(8,1));
+        System.out.println("8x2= " +  myMethod(8,2));
+        System.out.println("8x3= " +  myMethod(8,3));
+        System.out.println("8x4= " +  myMethod(8,4));
+        System.out.println("8x5= " +  myMethod(8,5));
+        System.out.println("8x6= " +  myMethod(8,6));
+        System.out.println("8x7= " +  myMethod(8,7));
+        System.out.println("8x8= " +  myMethod(8,8));
+        System.out.println("8x9= " +  myMethod(8,9));
+        System.out.println("8x10= " + myMethod(8,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table of 9 ===");
+        System.out.println("9x1= " +  myMethod(9,1));
+        System.out.println("9x2= " +  myMethod(9,2));
+        System.out.println("9x3= " +  myMethod(9,3));
+        System.out.println("9x4= " +  myMethod(9,4));
+        System.out.println("9x5= " +  myMethod(9,5));
+        System.out.println("9x6= " +  myMethod(9,6));
+        System.out.println("9x7= " +  myMethod(9,7));
+        System.out.println("9x8= " +  myMethod(9,8));
+        System.out.println("9x9= " +  myMethod(9,9));
+        System.out.println("9x10= " + myMethod(9,10));
+    }
+}
+
+public class Multi_Table{
+    static int myMethod(int x ,int y){
+        return x*y;
+    }
+    public static void main(String[] args){
+        System.out.println("=== Multipication Table of 10 ===");
+        System.out.println("10x1= " +  myMethod(10,1));
+        System.out.println("10x2= " +  myMethod(10,2));
+        System.out.println("10x3= " +  myMethod(10,3));
+        System.out.println("10x4= " +  myMethod(10,4));
+        System.out.println("10x5= " +  myMethod(10,5));
+        System.out.println("10x6= " +  myMethod(10,6));
+        System.out.println("10x7= " +  myMethod(10,7));
+        System.out.println("10x8= " +  myMethod(10,8));
+        System.out.println("10x9= " +  myMethod(10,9));
+        System.out.println("10x10= " + myMethod(10,10));
+    }
+}
